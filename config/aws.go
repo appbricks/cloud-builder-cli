5b3c38fe6f53f152c4ab180f628ea7c98954144f
Mevan Samaratunga <mevansam@gmail.com>
2021-06-14 10:51:46 -0400

    aws config constants

---
+++ config/aws.go
@@ -0,0 +1,13 @@
+package config
+
+const AWS_COGNITO_REGION = ""
+const AWS_COGNITO_USER_POOL_ID = ""
+
+const CLIENT_ID = ""
+const CLIENT_SECRET = ""
+const AUTH_URL = ""
+const TOKEN_URL = ""
+const USER_INFO_URL = ""
+
+const AWS_APPSYNC_REGION = ""
+const AWS_USERSPACE_API_URL = ""
