fe42b504d37ab8bbbc13ad67c60ada885b6510b3
Mevan Samaratunga <mevansam@gmail.com>
2022-03-20 22:24:36 -0400

    uncommented code targeted build for dev

--- scripts/build.sh
+++ scripts/build.sh
@@ -131,8 +131,8 @@ else
   build_timestamp=$(date +'%B %d, %Y at %H:%M %Z')
 
   # build release binaries for all supported architectures
-  # build "darwin" "amd64"
-  # build "linux" "amd64"
-  # build "linux" "arm64"
+  build "darwin" "amd64"
+  build "linux" "amd64"
+  build "linux" "arm64"
   build "windows" "amd64"
 fi
